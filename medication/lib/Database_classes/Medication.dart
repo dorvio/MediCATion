c4e87f177fe9962a3dffae10f510dddf161f7477
dorvio <dorokow599@student.polsl.pl>
2023-10-29 21:33:06 +0100

    added medication diaplay and expanded floating button

--- medication/lib/Database_classes/Medication.dart
+++ medication/lib/Database_classes/Medication.dart
@@ -6,12 +6,14 @@ class Medication {
   String medication;
   int type;
   String description;
+  bool forAnimal;
 
   Medication({
     required this.medicationId,
     required this.medication,
     required this.type,
     required this.description,
+    required this.forAnimal,
   });
 
   Medication copyWith({
@@ -19,12 +21,14 @@ class Medication {
     String? medication,
     int? type,
     String? description,
+    bool? forAnimal,
   }) {
     return Medication(
       medicationId : medicationId ?? this.medicationId,
       medication : medication ?? this.medication,
       type : type ?? this.type,
       description : description ?? this.description,
+      forAnimal : forAnimal ?? this.forAnimal,
     );
   }
 }
--- medication/lib/Database_classes/Usage.dart
+++ medication/lib/Database_classes/Usage.dart
@@ -5,21 +5,25 @@ class Usage {
   String usageId;
   String medicationId;
   String profileId;
+  String medicationName;
 
   Usage({
     required this.usageId,
     required this.medicationId,
+    required this.medicationName,
     required this.profileId,
   });
 
   Usage copyWith({
     String? usageId,
     String? medicationId,
+    String? medicationName,
     String? profileId,
   }) {
     return Usage(
       usageId : usageId ?? this.usageId,
       medicationId : medicationId ?? this.medicationId,
+      medicationName : medicationName ?? this.medicationName,
       profileId : profileId ?? this.profileId,
     );
   }
